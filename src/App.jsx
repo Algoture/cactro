e5d0406a611264859cdb186bce6c491d7f0a1950
Umesh Nagare <umeshnagare27@gmail.com>
2025-05-25 13:44:48 +0530

    done : insta story feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,90 @@
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from "react";
+import { stories } from "./data/stories";
+import clsx from "clsx";
 
 function App() {
+  const [storyId, setStoryId] = useState(null);
+  const [allStories] = useState(stories);
+  const [viewed, setViewed] = useState([]);
+  const selectedStory = allStories.find((s) => s.id === storyId);
+  const [showStory, setShowStory] = useState(false);
+
+  useEffect(() => {
+    if (!selectedStory) return;
+    setShowStory(false);
+    const delay = setTimeout(() => setShowStory(true), 50);
+    setViewed((prev) =>
+      prev.includes(selectedStory.id) ? prev : [...prev, selectedStory.id]
+    );
+    return () => clearTimeout(delay);
+  }, [storyId]);
+
+  useEffect(() => {
+    if (!selectedStory) return;
+    const timer = setTimeout(() => {
+      handleNext();
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [selectedStory]);
+
+  const handlePrevious = () => {
+    const currentIndex = allStories.findIndex((s) => s.id === storyId);
+    if (currentIndex > 0) {
+      setStoryId(allStories[currentIndex - 1].id);
+    }
+  };
+
+  const handleNext = () => {
+    const currentIndex = allStories.findIndex((s) => s.id === storyId);
+    if (currentIndex < allStories.length - 1) {
+      setStoryId(allStories[currentIndex + 1].id);
+    } else {
+      setStoryId(null);
+    }
+  };
+
   return (
-    <main className="flex items-center justify-center h-screen">
-      <div className="min-h-screen  flex flex-col items-start ml-1 md:ml-3 sm:ml-1 space-y-1 justify-center w-full">
-        <h1 className="text-3xl font-bold leading-tight  tracking-tight">
-          ShadCN + Vite + React + JavaScript
-        </h1>
-        <p className="text-lg text-muted-foreground">
-          A template, fast, and modern UI setup using ShadCN and Vite.
-        </p>
-        <a href="https://github.com/Algoture/vite-template" target="_blank">
-          <Button variant="default">Github</Button>
-        </a>
+    <main className="w-full h-screen relative overflow-hidden">
+      <div className="bg-gray-100 p-2 w-full items-center storyDiv whitespace-nowrap overflow-x-auto overflow-y-hidden">
+        {selectedStory && (
+          <div
+            className={clsx(
+              "absolute left-0 top-14 w-full h-full bg-center bg-cover transition-opacity duration-500",
+              showStory ? "opacity-100" : "opacity-0"
+            )}>
+            <img
+              src={selectedStory.img}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+            <button
+              className="absolute left-4 top-0 h-screen w-10"
+              onClick={handlePrevious}></button>
+            <button
+              className="absolute right-4 top-0 h-screen w-10"
+              onClick={handleNext}></button>
+          </div>
+        )}
+
+        {stories.map((story) => (
+          <div
+            key={story.id}
+            onClick={() => setStoryId(story.id)}
+            className={clsx(
+              "size-10 mr-1 inline-block cursor-pointer rounded-full border-[1.5px]",
+              story.id === storyId
+                ? "border-blue-500"
+                : viewed.includes(story.id)
+                ? "border-gray-200"
+                : "border-red-500"
+            )}>
+            <img
+              src={story.img}
+              alt={`Story ${story.id}`}
+              className="rounded-full object-cover w-full h-full"
+            />
+          </div>
+        ))}
       </div>
     </main>
   );
---
+++ src/data/stories.js
@@ -0,0 +1,42 @@
+export const stories = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1695384287398-5781a1f51c31?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1528109688329-6a043167789d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 4,
+        img: "https://images.unsplash.com/photo-1618697371651-b203d4c3bac5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 5,
+        img: "https://images.unsplash.com/photo-1634455257140-b5ab16f0770e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 6,
+        img: "https://images.unsplash.com/photo-1658281521311-d42fdfe2e8a1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 7,
+        img: "https://plus.unsplash.com/premium_photo-1739981551308-e45934e03726?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 8,
+        img: "https://images.unsplash.com/photo-1678107187821-b298667c9677?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 9,
+        img: "https://plus.unsplash.com/premium_photo-1711987278682-e14d1ae26634?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzJ8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+    {
+        id: 10,
+        img: "https://images.unsplash.com/photo-1623004665009-d24ade8dd82f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fHVuc3BhbHNofGVufDB8fDB8fHww",
+    },
+]
